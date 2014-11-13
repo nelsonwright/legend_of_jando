e8e0dee9302336c734f2b0dd2d299f093a93b418
Nelson Wright <nwright@zipcar.co.uk>
2014-11-13 15:23:59 +0000

    Added grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+/*    jasmine: {
+      test: {
+        options: {
+          specs: 'build/test.js',
+          outfile: 'build/test/SpecRunner.html'
+        }
+      } 
+    }, */
+    
+    browserify: {
+      release: {
+        src: 'scripts/**/*.js',
+        dest: 'build/main.js'
+      },
+      debug: {
+        src: 'spec/**/*.js',
+        dest: 'build/test.js',
+        options: {
+          browserifyOptions: {
+            debug: true
+          }
+        }
+      }
+    },
+    
+    
+  });
+  
+ // grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-browserify');
+ // grunt.loadNpmTasks('grunt-contrib-watch');
+ // grunt.loadNpmTasks('grunt-shell');
+  
+//  grunt.registerTask('spec', ['browserify:debug', 'jasmine']);
+//  grunt.registerTask('spec:debug', ['browserify:debug', 'jasmine:test:build', 'shell:openRunner']);
+  grunt.registerTask('build', ['browserify:release']);
+  
+};
